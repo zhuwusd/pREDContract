1770443b0a9164dd97da7d4e86ac1adf98630804
Tidi.Zhu <zhuwusd@163.com>
2022-02-19 13:28:37 +0800

    更改可以在浏览器上调试Vue代码

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  BASE_API_URL:'"http://localhost:8000"',
+  BASE_API_URL:'"http://localhost:8080"',
   NODE_ENV: '"development"',
   WEB_ENV:false
 })
